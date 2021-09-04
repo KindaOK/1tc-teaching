e91fb0a8c2a28237018ce029da1273bb69ee53b0
KindaOK <27974340+KindaOK@users.noreply.github.com>
2021-09-03 20:43:22 -0400

    Update NoteManagerSkeleton.js
    lol whoops forgot to delete some code.

--- src/components/NotesManagerSkeleton/NoteManagerSkeleton.js
+++ src/components/NotesManagerSkeleton/NoteManagerSkeleton.js
@@ -5,24 +5,6 @@ import Note from "./NoteSkeleton/NoteSkeleton";
 import "./NoteManagerSkeleton.css";
 
 function NoteManagerSkeleton() {
-  // only run once at component creation
-  useEffect(() => {
-    // try to load any saved notes
-    //  if there are no saved notes, then parse an empty array
-    const savedNotes = JSON.parse(localStorage.getItem("notes") ?? "[]");
-    savedNotes.forEach(
-      (note) => (note.dateCreated = new Date(note.dateCreated))
-    );
-    if (savedNotes?.length) {
-      setNotes(savedNotes);
-    }
-  }, []);
-
-  // every time the notes are updated, save to local storage
-  //  it doesn't matter what updates them
-  useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
 
   return (
     <>
